f51e6d4cf35266312eafd8d04203e42d9ec783cf
Yusuke Miura <yusukemiura34c@gmail.com>
2022-05-04 18:41:54 +0900

    second commit

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import HeaderCommon from "@/components/HeaderCommon.vue";
-import { ref } from "vue";
-let headerHeightStyle = ref<string>();
+import { ref, onMounted } from "vue";
+
+const headerRef = ref<InstanceType<typeof HeaderCommon>>();
+const screenHeight = ref<number>(window.innerHeight);
+console.log(screenHeight);
+let headerHeightStyle = ref<string>("13.6419vh");
+let pageHeightStyle = ref<string>("86.3581vh");
+// onMounted(() => {
+//   const headerHeight = ref<number>(headerRef.value?.$el.clientHeight);
+//   const headerHeightVh = ref<number>(
+//     (headerHeight.value / screenHeight.value) * 100
+//   );
+//   headerHeightStyle.value = headerHeightVh.value + "vh";
+//   const pageHeightVh = ref<number>(100 - headerHeightVh.value);
+//   pageHeightStyle.value = pageHeightVh.value + "vh";
+//   console.log(pageHeightStyle.value);
+//   console.log(headerHeightStyle);
+// });
 </script>
 
 <template>
-  <HeaderCommon ref="headerRef" :style="{ height: headerHeightStyle }" />
-  <RouterView />
+  <div>
+    <HeaderCommon ref="headerRef" :style="{ height: headerHeightStyle }" />
+    <RouterView :style="{ height: pageHeightStyle }" />
+  </div>
 </template>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts"></script>
-
-<template>
-  <div>hello world</div>
+<template class="">
+  <div class="h-full bg-gray-700 p-5">
+    <div class="top-box">
+      <div class="flex mx-10 p-5 bg-indigo-200 justify-center items-center">
+        <div>hello world</div>
+      </div>
+    </div>
+    <div class="bottom-box">
+      <div class="m-4 bg-indigo-400">hi</div>
+    </div>
+  </div>
 </template>
+
+<style>
+.top-box {
+  height: 30%;
+}
+.bottom-box {
+  height: 70%;
+}
+</style>
