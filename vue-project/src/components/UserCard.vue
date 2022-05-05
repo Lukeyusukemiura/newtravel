b8b0962c5f963ec8bdac8367edf806c4c00d27df
Yusuke Miura <yusukemiura34c@gmail.com>
2022-05-05 16:10:07 +0900

    made something

---
+++ vue-project/src/components/UserCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a href="#" class="m-3">
+    <div class="bg-white card overflow-hidden border rounded-lg">
+      <div class="relative pb-52">
+        <img class="absolute h-full w-full object-cover" src="#" />
+      </div>
+      <div class="p-5 text-center overflow-hidden">
+        <h4 class="text-2xl font-bold mb-2">text</h4>
+        <p>text</p>
+      </div>
+    </div>
+  </a>
+</template>
+
+<style>
+.top-font {
+  font-family: serif;
+}
+.card {
+  height: 400px;
+  width: 400px;
+}
+</style>
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,22 +1,147 @@
 <script setup lang="ts"></script>
 <template class="">
-  <div class="h-full bg-gray-700 p-5">
-    <div class="top-box">
-      <div class="flex mx-10 p-5 bg-indigo-200 justify-center items-center">
-        <div>hello world</div>
+  <div class="h-full bg-gray-700 p-3">
+    <div class="flex content-box">
+      <div class="left-box flex-column justify-center items-center overs">
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card overflow-hidden border rounded-lg">
+              <div class="relative pb-44">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">I travel for the nature</h4>
+                <p>
+                  This is the photo when I went to Australia to feel the nature.
+                  Location is Tasmania where my best friend lives.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card overflow-hidden border rounded-lg">
+              <div class="relative pb-44">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">text</h4>
+                <p>text</p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card overflow-hidden border rounded-lg">
+              <div class="relative pb-44">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">text</h4>
+                <p>text</p>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
-    </div>
-    <div class="bottom-box">
-      <div class="m-4 bg-indigo-400">hi</div>
+      <div
+        class="right-box flex-column justify-center rounded-lg items-center overs"
+      >
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card-long overflow-hidden border rounded-lg">
+              <div class="relative pb-80">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">I travel for the nature</h4>
+                <p>
+                  This is the photo when I went to Australia to feel the nature.
+                  Location is Tasmania where my best friend lives.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card-long overflow-hidden border rounded-lg">
+              <div class="relative pb-80">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">I travel for the nature</h4>
+                <p>
+                  This is the photo when I went to Australia to feel the nature.
+                  Location is Tasmania where my best friend lives.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div>
+          <a href="#" class="m-1">
+            <div class="bg-white card-long overflow-hidden border rounded-lg">
+              <div class="relative pb-80">
+                <img
+                  class="absolute h-full w-full object-cover"
+                  src="../image/photo.png"
+                />
+              </div>
+              <div class="p-5 text-center overflow-hidden">
+                <h4 class="text-2xl font-bold mb-2">I travel for the nature</h4>
+                <p>
+                  This is the photo when I went to Australia to feel the nature.
+                  Location is Tasmania where my best friend lives.
+                </p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.top-box {
-  height: 30%;
+.content-box {
+  width: 100%;
+  height: 100%;
 }
-.bottom-box {
-  height: 70%;
+.left-box {
+  width: 30%;
+  height: 100%;
+}
+.right-box {
+  width: 70%;
+  height: 100%;
+}
+.overs {
+  overflow: scroll;
+}
+.card {
+  height: 300px;
+  width: 400px;
+}
+.card-long {
+  height: 500px;
+  width: 100%;
 }
 </style>
