ebc2cad1912c72d9cbb372c52a03dbd4213d1083
Yusuke Miura <yusukemiura34c@gmail.com>
2022-05-09 21:57:37 +0900

    firebase and account page

--- vue-project/src/components/HeaderCommon.vue
+++ vue-project/src/components/HeaderCommon.vue
@@ -4,7 +4,11 @@
   <div class="flex px-4 py-8 bg-gray-700 items-center justify-center">
     <div class="px-2 font-bold text-5xl text-white top-font">Pegasus</div>
   </div>
-  <div class="px-2 font-bold text-5xl text-white top-font">Pegasus</div>
+  <RouterLink
+    class="absolute top-10 left-3 font-bold text-3xl text-white top-font px-2"
+    to="/account"
+    >Account</RouterLink
+  >
   <div class="absolute top-10 right-1">
     <input class="text" />
     <button
---
+++ vue-project/src/views/AccountPage.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="content-box">
+    <div class="top-box bg-indigo-200 p-5 flex justify-center items-center">
+      <div class="top-inside-box bg-white flex justify-around items-center">
+        <div>hel</div>
+        <div>lo</div>
+      </div>
+    </div>
+    <div class="bottom-box bg-red-200 p-5"></div>
+  </div>
+</template>
+<style>
+.content-box {
+  width: 100%;
+  height: 100%;
+}
+.top-box {
+  height: 40%;
+  width: 100%;
+}
+.top-inside-box {
+  width: 70%;
+  height: 100%;
+}
+.bottom-box {
+  height: 60%;
+  width: 100%;
+}
+</style>
